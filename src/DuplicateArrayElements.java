22fa3ffe5cff767c16df27c7760c91cf47830f64
lakshmiprasanna6715 <prasannal6715@gmail.com>
2025-04-19 23:35:44 +0530

    second commit

--- src/DuplicateArrayElements.java
+++ src/DuplicateArrayElements.java
@@ -1,20 +1,29 @@
+import java.util.Scanner;
 
 public class DuplicateArrayElements {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] a= {1,4,8,5,6,9,5,1,7};
-		
-		System.out.println("Duplicate array of elements is");
-		
-		for(int i=0;i<a.length;i++) {
-			for(int  j=i+1;j<a.length;j++) {
-				if(a[i]==a[j]) {
-					System.out.println(a[i]);
-					break;
-				}
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter the number of elements in the array:");
+        int n = sc.nextInt(); // user defines array size
+        int[] a = new int[n];
+
+        System.out.println("Enter " + n + " array elements:");
+        for(int i = 0; i < n; i++) {
+            a[i] = sc.nextInt(); // store user input in array
+        }
+
+        System.out.println("Duplicate elements in the array:");
+        for(int i = 0; i < a.length; i++) {
+            for(int j = i + 1; j < a.length; j++) {
+                if(a[i] == a[j]) {
+                    System.out.println(a[i]);
+                    break;
+                }
+            }
+        }
+
+        sc.close();
+    }
 }
